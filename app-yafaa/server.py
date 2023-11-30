ac5980f68aeadc3a7625348eb64c28b97f090c07
salaheddine-ghannouch <salahghannouch@gmail.com>
2023-12-01 00:00:32 +0100

    add homepage to flask project

--- app-yafaa/server.py
+++ app-yafaa/server.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import render_template
+from datetime import datetime
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index_home():
+    date_var = datetime.now().year
+
+    additional_data = {
+        'current_date': date_var
+    }
+    return render_template('index.html', **additional_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
