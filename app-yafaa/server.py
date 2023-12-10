0f6624142293c6e0afbc3f8d28aa48fd4b507c8b
salaheddine-ghannouch <salahghannouch@gmail.com>
2023-12-10 21:36:14 +0100

    add tkharmisa of aymane

--- app-yafaa/server.py
+++ app-yafaa/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask import render_template
 from flask import request
 from datetime import datetime
@@ -18,7 +18,7 @@ def index_home():
     return render_template('index.html', **additional_data)
 
 #! ****************** Fixtures Page
-@app.route('/fixtures')
+@app.route('/fixt')
 def fixtures_func():
     date_var = datetime.now().year
     additional_data = {
@@ -46,171 +46,109 @@ def stats_func():
     }
     return render_template('stats.html', **additional_data)
 
-#! ****************** eng Page
-@app.route('/eng', methods=['GET', 'POST'])
-def eng_func():
 
-    #? Get Current Year
-    date_var = datetime.now().year
-    #? Get season
-    years = eloClub.generate_years()
-    #? Tretement for elo data
-    if request.method == 'POST':
-        selected_year = int(request.form.get('season'))
-        elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ENG-Premier League')
-    else:
-        # Default to the current year
-        selected_year = date_var
-        elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ENG-Premier League')
-
-    #? figure displaying : 
-    fig = eloClub.plot_elo_histogram(elo_eng, 'ENG-Premier League')
-    graph_json = fig
-
-    #? send Data here : 
-    additional_data = {
-        'current_date': date_var,
-        'active': 'side-bar__list-item--active',
-        'active_link': 'eng',
-        'season_available': years,
-        'season_selected': selected_year,
-        'elo_data': elo_eng.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
-        'graph_json':graph_json
-    }
-
-    return render_template('fixt/eng.html', **additional_data)
+#! --------------- process all cases : 
+@app.route('/<path:route_argument>', methods=['GET', 'POST'])
+def dynamic_route(route_argument):
+    
+    # Split the route_argument into navbar_selected and league_code
+    parts = route_argument.split('/')
+    navbar_selected = parts[0]
+    league_code = parts[1] if len(parts) > 1 else None
 
 
-#! ****************** fra Page
-@app.route('/fra', methods=['GET', 'POST'])
-def fra_func():
-    #? Get Current Year
-    date_var = datetime.now().year
-    #? Get season
-    years = eloClub.generate_years()
-    #? Tretement for elo data
-    if request.method == 'POST':
-        selected_year = int(request.form.get('season'))
-        elo_fra = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'FRA-Ligue 1')
-    else:
-        # Default to the current year
-        selected_year = date_var
-        elo_fra = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'FRA-Ligue 1')
+    #? ------------ case for elo navbar selected : 
+    if navbar_selected =='elo' and (league_code in ('eng','fra','ger','esp','ita')):
+        #? Get Current Year
+        date_var = datetime.now().year
+        league_name = ''
+        #? Get season
+        years = eloClub.generate_years()
+        #? Tretement for elo data
+        if league_code =='eng':
+            league_name = 'ENG-Premier League'
+        if league_code =='fra':
+            league_name = 'FRA-Ligue 1'
+        if league_code =='ger':
+            league_name = 'GER-Bundesliga'
+        if league_code =='esp':
+            league_name = 'ESP-La Liga'
+        if league_code =='ita':
+            league_name = 'ITA-Serie A'
+        
+        if request.method == 'POST':
+            selected_year = int(request.form.get('season'))
+            elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year,league_name )
+        else:
+            # Default to the current year
+            selected_year = date_var
+            elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ENG-Premier League')
 
-    #? figure displaying : 
-    fig = eloClub.plot_elo_histogram(elo_fra, 'FRA-Ligue 1')
-    graph_json = fig
+        #? figure displaying : 
+        fig = eloClub.plot_elo_histogram(elo_eng, league_name)
+        graph_json = fig
 
-    #? send Data here : 
-    additional_data = {
-        'current_date': date_var,
-        'active': 'side-bar__list-item--active',
-        'active_link': 'fra',
-        'season_available': years,
-        'season_selected': selected_year,
-        'elo_data': elo_fra.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
-        'graph_json':graph_json
-    }
-    return render_template('fixt/fra.html', **additional_data)
+        #? send Data here : 
+        additional_data = {
+            'current_date': date_var,
+            'active': 'side-bar__list-item--active',
+            'active_link': league_code,
+            'season_available': years,
+            'season_selected': selected_year,
+            'elo_data': elo_eng.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
+            'graph_json':graph_json
+        }
+    
+    #?----------------- case of fixtures navbar selected
+    if navbar_selected =='fixt' and (league_code in ('eng','fra','ger','esp','ita')):
+        #? Get Current Year
+        date_var = datetime.now().year
+        league_name = ''
+        #? Get season
+        years = eloClub.generate_years()
+        #? Tretement for elo data
+        if league_code =='eng':
+            league_name = 'ENG-Premier League'
+        if league_code =='fra':
+            league_name = 'FRA-Ligue 1'
+        if league_code =='ger':
+            league_name = 'GER-Bundesliga'
+        if league_code =='esp':
+            league_name = 'ESP-La Liga'
+        if league_code =='ita':
+            league_name = 'ITA-Serie A'
+        
+        if request.method == 'POST':
+            selected_year = int(request.form.get('season'))
+            elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year,league_name )
+        else:
+            # Default to the current year
+            selected_year = date_var
+            elo_eng = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ENG-Premier League')
 
+        #? figure displaying : 
+        fig = eloClub.plot_elo_histogram(elo_eng, league_name)
+        graph_json = fig
 
-#! ****************** ger Page
-@app.route('/ger', methods=['GET', 'POST'])
-def ger_func():
-    #? Get Current Year
-    date_var = datetime.now().year
-    #? Get season
-    years = eloClub.generate_years()
-    #? Tretement for elo data
-    if request.method == 'POST':
-        selected_year = int(request.form.get('season'))
-        elo_ger = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'GER-Bundesliga')
-    else:
-        # Default to the current year
-        selected_year = date_var
-        elo_ger = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'GER-Bundesliga')
+        #? send Data here : 
+        additional_data = {
+            'current_date': date_var,
+            'active': 'side-bar__list-item--active',
+            'active_link': league_code,
+            'season_available': years,
+            'season_selected': selected_year,
+            # 'elo_data': elo_eng.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
+            # 'graph_json':graph_json
+        }
 
-    #? figure displaying : 
-    fig = eloClub.plot_elo_histogram(elo_ger, 'GER-Bundesliga')
-    graph_json = fig
-
-    #? send Data here : 
-    additional_data = {
-        'current_date': date_var,
-        'active': 'side-bar__list-item--active',
-        'active_link': 'ger',
-        'season_available': years,
-        'season_selected': selected_year,
-        'elo_data': elo_ger.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
-        'graph_json':graph_json
-    }
-    return render_template('fixt/ger.html', **additional_data)
-
-
-#! ****************** esp Page
-@app.route('/esp', methods=['GET', 'POST'])
-def esp_func():
-    #? Get Current Year
-    date_var = datetime.now().year
-    #? Get season
-    years = eloClub.generate_years()
-    #? Tretement for elo data
-    if request.method == 'POST':
-        selected_year = int(request.form.get('season'))
-        elo_esp = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ESP-La Liga')
-    else:
-        # Default to the current year
-        selected_year = date_var
-        elo_esp = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ESP-La Liga')
-
-    #? figure displaying : 
-    fig = eloClub.plot_elo_histogram(elo_esp, 'ESP-La Liga')
-    graph_json = fig
-
-    #? send Data here : 
-    additional_data = {
-        'current_date': date_var,
-        'active': 'side-bar__list-item--active',
-        'active_link': 'esp',
-        'season_available': years,
-        'season_selected': selected_year,
-        'elo_data': elo_esp.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
-        'graph_json':graph_json
-    }
-    return render_template('fixt/esp.html', **additional_data)
+    #? -------------- process the else case : 
+    if navbar_selected not in ('elo','fixt','stats'):
+        return redirect(url_for('index_home'))
 
 
-#! ****************** ita Page
-@app.route('/ita', methods=['GET', 'POST'])
-def ita_func():
-     #? Get Current Year
-    date_var = datetime.now().year
-    #? Get season
-    years = eloClub.generate_years()
-    #? Tretement for elo data
-    if request.method == 'POST':
-        selected_year = int(request.form.get('season'))
-        elo_ita = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ITA-Serie A')
-    else:
-        # Default to the current year
-        selected_year = date_var
-        elo_ita = eloClub.process_clubs_elo_for_year_and_league(selected_year, 'ITA-Serie A')
+    return render_template(navbar_selected+'/'+league_code+'.html', **additional_data)
 
-    #? figure displaying : 
-    fig = eloClub.plot_elo_histogram(elo_ita, 'ITA-Serie A')
-    graph_json = fig
 
-    #? send Data here : 
-    additional_data = {
-        'current_date': date_var,
-        'active': 'side-bar__list-item--active',
-        'active_link': 'ita',
-        'season_available': years,
-        'season_selected': selected_year,
-        'elo_data': elo_ita.to_dict(orient='records'),  # Convert DataFrame to a list of dictionaries
-        'graph_json':graph_json
-    }
-    return render_template('fixt/ita.html', **additional_data)
 
 
 if __name__ == '__main__':
